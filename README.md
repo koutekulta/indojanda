60d7f29d15de74c3f63180e120437e07e10b318b
koutekulta <koutekulta@gmail.com>
2025-06-28 02:34:46 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# indojanda
-indojanda
+# [indojanda](https://indojanda.pages.dev)
---
+++ src/utils/site.ts
@@ -0,0 +1,4 @@
+export const nama = 'Indo Janda';
+export const desk = 'Video bokep indo jepang viral ngentot colmek bocil jilbab smp tante.';
+export const url = 'https://indojanda.pages.dev';
+export const terbit = '2025-06-27T00:01:03Z';
